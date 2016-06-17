e08b8ee5f2848c3ff6a4928d82e054ed77a490b3
Israel Ogbole <iogbole@Israels-MacBook-Pro.local>
2016-06-17 16:15:44 +0100

    Added flash message

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'plainpage/index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'plainpage#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
