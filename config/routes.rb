bdfc51dfd0e1ecb96fd9d35fcd8eb2549ae8645e
ggomagundan <pbs52@hanmail.net>
2016-12-09 17:26:08 +0900

    chagne root page to dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'plainpage#index'
+   root 'plainpage#dashboard'
    get "dashboard", to: 'plainpage#dashboard'
 
   # Example of regular route:
